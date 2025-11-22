a0270ef88211d8689017dd7a5df3b19b52f2c56e
Yoo-Su-Hyeon <jjongari01@naver.com>
2025-11-23 06:22:02 +0900

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,8 @@
 // src/api/config.js
-// 👉 백엔드 기본 주소
 //export const API_BASE_URL =
   //process.env.REACT_APP_API_BASE_URL ||
   //"https://preaortic-paratactically-marti.ngrok-free.dev";
 
 
 export const API_BASE_URL = "https://preaortic-paratactically-marti.ngrok-free.dev";
+
